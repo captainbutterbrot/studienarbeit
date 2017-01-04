7176bd1f0088c49d9ed8c31aee49f910ea8ba785
Hannah <hannah.albrecht@hs-augsburg.de>
2017-01-04 18:50:39 +0100

    IT FUCKING WORKS

--- studienarbeit/mmprog/web/js/app/model/modelPlayer.js
+++ studienarbeit/mmprog/web/js/app/model/modelPlayer.js
@@ -14,6 +14,8 @@ define
       this.x_start  = p_init.pos.x;
       this.y_start  = p_init.pos.y;
 
+      this.reset(); // initializes further attributes
+
     }
 
     ModelPlayer.prototype =
@@ -30,7 +32,46 @@ define
         show: function ()
               {
                 this.visible = true;
+              },
+        stop:
+          function()
+          { this.vx = 0;
+            this.vy = 0;
+
+            this.ax = 0;
+            this.ay = 0;
+          },
+
+        start:
+          function(p_direction)
+          {
+            // react only if the paddle is visible not already moving
+            if (this.visible === true &&
+              this.vx === 0 && this.vy === 0
+            )
+            {
+              switch (p_direction)
+              {
+                case "left":
+                  this.vx = -this.vx_start;
+                  this.ax = -this.ax_start;
+                  break;
+                case "right":
+                  this.vx =  this.vx_start;
+                  this.ax =  this.ax_start;
+                  break;
+
+                case "up":
+                  this.vy = -this.vy_start;
+                  this.ay = -this.ay_start;
+                  break;
+                case "down":
+                  this.vy =  this.vy_start;
+                  this.ay =  this.ay_start;
+                  break;
               }
+            }
+          }
       };
 
     return ModelPlayer;
